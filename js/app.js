504d6c64aff8cd76afa43a2c01298b454927741b
Kaleb berhane <kakuj424@gmail.com>
2023-02-21 15:34:57 -0700

    achievement finished

--- js/app.js
+++ js/app.js
@@ -4,30 +4,29 @@
 const timeOfDay = 'afternoon';
 const sleepy = true;
 
-if (timeOfDay === 'afternoon') {
-  if (sleepy) {
-    console.log('Time to have my afternoon nap!');
-  }
+if ('timeOfDay && sleepy') {
+  
+  console.log('Time to have my afternoon nap!');
+
 }
 
 // Example 2: OR (||) Operator
 // Using a logical OR operator (||), refactor the following code into ONE if statement that contains ONE console log.
 
 const mood = 'happy';
+const appetite = 'hungry';
 
-if (mood === 'stressed') {
+if (mood || appetite) {
+  
   console.log('Pass the potato chips!');
-}
 
-if (mood === 'hungry') {
-  console.log('Pass the potato chips!');
 }
 
 // Example 3: Logical NOT (!) Operator
 // Using a logical NOT operator (!), refactor the condition of the following if statement so that the statement is logged to the console correctly.
 
-const cloudy = true;
+const cloudy = false;
 
-if (cloudy) {
+if (!cloudy) {
   console.log('Put on sun glasses.');
 }
